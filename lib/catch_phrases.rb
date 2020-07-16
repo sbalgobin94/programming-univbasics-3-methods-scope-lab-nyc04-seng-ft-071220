e390599fd8a8c243cf340d01829a60767cfce602
Samantha Balgobin <sbalgobin94@gmail.com>
2020-07-16 16:15:10 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,9 +1,12 @@
 def mario
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  toadstool
   puts phrase
 end
 
 def toadstool
  mario
  puts status
+ phrase = 'It's-a me, Mario!'
 end
+
